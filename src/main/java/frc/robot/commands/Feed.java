b1ab00581499e703ac65215dc0890e81e1a0a3c4
SophWed1 <111957156+SophWed1@users.noreply.github.com>
2023-01-11 16:03:54 +1100

    Feeder project assigned by Asaph

---
+++ src/main/java/frc/robot/commands/Feed.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Feeder;
+
+public class Feed extends CommandBase {
+  /** Creates a new Feed. */
+
+  private final Feeder feeder;
+
+  public Feed(Feeder feeder) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.feeder = feeder;
+    addRequirements(feeder);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    feeder.moveFeeder(0.5);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    feeder.stopFeeder();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class Feeder extends SubsystemBase {
+
+  VictorSPX feederBack;
+  VictorSPX feederFront;
+
+  /** Creates a new Feeder. */
+  public Feeder() {
+    feederBack = new VictorSPX(6);
+    feederFront = new VictorSPX(5);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void moveFeeder(double speed){
+    feederBack.set(VictorSPXControlMode.PercentOutput, speed);
+    feederFront.set(VictorSPXControlMode.PercentOutput, speed);
+
+  }
+
+  public void stopFeeder(){
+    feederBack.set(VictorSPXControlMode.PercentOutput, 0);
+    feederFront.set(VictorSPXControlMode.PercentOutput, 0);
+
+  }
+
+}
